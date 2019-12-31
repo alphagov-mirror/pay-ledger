62504e19e8f0bff428a03200362aa2301de26447
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2019-12-31 17:53:56 +0000

    PP-5978: Implement /v1/report/performance-report endpoint
    I saw no need to implement a Service class here, so the
    PerformanceReportResource calls the PerformanceReportDao directly.

--- src/main/java/uk/gov/pay/ledger/report/entity/PerformanceReportEntity.java
+++ src/main/java/uk/gov/pay/ledger/report/entity/PerformanceReportEntity.java
@@ -1,10 +1,18 @@
 package uk.gov.pay.ledger.report.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 
 public class PerformanceReportEntity {
+
+    @JsonProperty("total_volume")
     private final long totalVolume;
+
+    @JsonProperty("total_amount")
     private final BigDecimal totalAmount;
+
+    @JsonProperty("average_amount")
     private final BigDecimal averageAmount;
 
     public PerformanceReportEntity(long totalVolume, BigDecimal totalAmount, BigDecimal averageAmount) {
